0baca0f81904f52eb554c629da6074344dd3ff80
Taejin Woo <88721626+squiduu@users.noreply.github.com>
2022-10-27 21:21:46 +0900

    Delete crows_pairs directory

--- crows_pairs/run_crows_pairs.sh
+++
@@ -1,8 +0,0 @@
-python run_crows_pairs.py \
-    --test_data_path ./data/crows_pairs_anonymized.csv \
-    --bias_type gender \
-    --model_name_or_path ../guidebias/out/bert_guidebias_ep1_seed0_num100-10 \
-    --model_name bert-base-uncased \
-    --output_dir ./out/ \
-    --run_name run00 \
-    --seed 0
