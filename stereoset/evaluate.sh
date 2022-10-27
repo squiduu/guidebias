dc6289fcc2f3fe63bfb533abd841e12206533e28
Taejin Woo <88721626+squiduu@users.noreply.github.com>
2022-10-27 21:22:11 +0900

    Delete stereoset directory

--- stereoset/evaluate.sh
+++
@@ -1,4 +0,0 @@
-python evaluation.py \
-    --predictions_file ./out/results/stereoset/stereoset_model-bert_guidebias_ep1_seed0_num100-10_type-gender_seed-0.json \
-    --predictions_dir ./out/results/stereoset/ \
-    --output_file ./out/results/stereoset/scores.json
